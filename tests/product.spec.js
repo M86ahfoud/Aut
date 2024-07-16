3848cfc4485e9b31deddf34620b728446fc83394
FIRST\_Mahfoud LAST\_HANI <[MY\_NAME@example.com](mailto:hanimahfoud@gmail.com)>
2024-07-16 16:50:29 +0200

    add comment

--- tests/product.spec.js
+++ tests/product.spec.js
@@ -18,10 +18,15 @@ test.describe('test produt list', async () => {
     })
 
     test('test navigate to product list', async () => {
+        // Navigate to the home page 
         await productInstance.goto();
+        // Verify that home page is visible successful
         await productInstance.checkHompage();
+        // Click on "Products button" and navigate to the product page
         await productInstance.navigateToProductPage();
+        // Verify that list of products is visible  
         await productInstance.visibleListProduct();
+        // Check the visibilité the detail of first product
         await productInstance.showDetailOfFirstProduct();
         await productInstance.detailVisibility();
         await productInstance.vérifyQuantity();
